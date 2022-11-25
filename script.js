0a6f602a5a35f099deb32417875327596d950383
kentoaiba0313 <akento1997@gmail.com>
2022-11-25 11:50:37 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementById("isekifirst").
+ addEventListener("click" ,function(){
+  this.textContent = "アンコールワットは12世紀に建てられたクメール王朝時代の遺跡です。実物はとても迫力があり壮大な遺跡ですが、壁の装飾といった細かいところまで美しく非常におすすめです。"
+ });
+
+ document.getElementById("isekisecond").
+ addEventListener("click" ,function(){
+  this.textContent = "古代ローマ帝国時代の遺跡群です。コロッセオやパンテオン神殿、フォロロマーノなど有名な遺跡がいくつもあり、歴史をあまり知らなくても楽しめます。"
+ });
+
+ document.getElementById("isekithird").
+ addEventListener("click" ,function(){
+  this.textContent = "14世紀半ばから18世紀年代半ばにかけて存在したアユタヤ王朝時代の遺跡群です。公園内とその周辺には数多くの遺跡があり、とても見ごたえがあります。"
+ });
+
+ document.getElementById("shizenfirst").
+ addEventListener("click" ,function(){
+  this.textContent = "サハラ砂漠はアフリカ大陸北部にある世界最大の砂漠です。山のような砂丘があたり一面に広がっており、一度は見るべき絶景です。また、夜は満点の星空を見ることが出来ます。"
+ });
+
+ document.getElementById("shizensecond").
+ addEventListener("click" ,function(){
+  this.textContent = "セブ島の周辺にはマクタン島など大小さまざまな島があり、美しい海やビーチを見ることが出来ます。セブ島に行った際は、島めぐりができるアイランドホッピングへの参加をおすすめします。"
+ });
+
+ document.getElementById("shizenthird").
+ addEventListener("click" ,function(){
+  this.textContent = "ナイアガラは世界三大瀑布に選ばれているとても迫力のある大きな滝です。写真のようにボートで滝の近くまで行くことが出来ます。"
+ });
+
+ document.getElementById("buildingfirst").
+ addEventListener("click" ,function(){
+  this.textContent = "モンサンミシェルは小さな島の上に造られた修道院です。外から見た修道院はもちろん、内部も映画のセットのように美しい建物です。夜はライトアップされてとても綺麗です。"
+ });
+
+ document.getElementById("buildingsecond").
+ addEventListener("click" ,function(){
+  this.textContent = "サンピエトロ大聖堂は、バチカン市国にあるカトリックの総本山です。ミケランジェロの最後の審判など、美術品も見ごたえがあります。ローマ市内にあるので、そちらとセットで観光ができます。"
+ });
+
+ document.getElementById("buildingthird").
+ addEventListener("click" ,function(){
+  this.textContent = "サグラダファミリアは、ガウディによって建てられた未完成の教会です。長年にわたって建築されていることもあり、とても壮大で豪華な教会です。近いうちに完成するそうなので、その前に行くことをおすすめします。"
+ });
+
